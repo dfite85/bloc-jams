16042a38ef19f034d99b5d98c005fac570c6d724
Dustin Fite <Dfite85@gmail.com>
2017-10-02 14:26:10 -0500

    bug fixes to lesson 10

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,31 +1,3 @@
-
-var buildCollectionItemTemplate = function () {
-  var template =
-    '<div class="collection-album-container column fourth">'
-    +   '   <img src="assets/images/album_covers/01.png"/>'
-    +   '   <div class="collection-album-info caption">'
-    +   '       <p>'
-    +   '           <a class="album-name" href="album.html"> The Colors </a>'
-    +   '           <br/>'
-    +   '           <a href="album.html"> Pablo Picasso </a>'
-    +   '           <br/>'
-    +   '           X songs'
-    +   '           <br/>'
-    +   '       </p>'
-    +   '   </div>'
-    +   '</div>';
-  return $(template);
-};
-$(window).load(function() {
-  var $collectionContainer = $('.album-covers');
-  $collectionContainer.empty();
-
-  for(var i = 0; i < 12; i++) {
-    var $newThumbNail = buildCollectionItemTemplate();
-    $collectionContainer.append($newThumbNail);
-  }
-});
-=======
  var collectionItemTemplate =
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
@@ -42,7 +14,7 @@ $(window).load(function() {
    + '</div>'
    ;
 
-window.onload = function() {
+ window.onload = function() {
      // #1
      var collectionContainer = document.getElementsByClassName('album-covers')[0];
      // #2
