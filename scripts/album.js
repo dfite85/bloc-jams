7cf482b96da1b2129fc1d5cb95394cb8ce5d53df
Dustin Fite <Dfite85@gmail.com>
2017-10-23 20:05:43 -0500

    lesson 19 assingment

--- scripts/album.js
+++ scripts/album.js
@@ -1,56 +1,13 @@
- var albumPicasso = {
-     title: 'The Colors',
-     artist: 'Pablo Picasso',
-     label: 'Cubism',
-     year: '1881',
-     albumArtUrl: 'assets/images/album_covers/01.png',
-     songs: [
-         { title: 'Blue', duration: '4:26' },
-         { title: 'Green', duration: '3:14' },
-         { title: 'Red', duration: '5:01' },
-         { title: 'Pink', duration: '3:21'},
-         { title: 'Magenta', duration: '2:15'}
-     ]
- };
- 
-var albumMarconi = {
-     title: 'The Telephone',
-     artist: 'Guglielmo Marconi',
-     label: 'EM',
-     year: '1909',
-     albumArtUrl: 'assets/images/album_covers/20.png',
-     songs: [
-         { title: 'Hello, Operator?', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21'},
-         { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15'}
-     ]
- };
-     // Dont judge me!!!!!
-var albumQueen = {
-     title: 'Queen Rock Montreal',
-     artist: 'Queen',
-     label: 'Mountain Studios',
-     year: '1982',
-     albumArtUrl: 'assets/images/album_covers/20.png',
-     songs: [
-         { title: 'Intro', duration: '1:59' },
-         { title: 'We Will ROck You', duration: '3:06' },
-         { title: 'Play The Game', duration: '3:57'},
-         { title: 'Somebody To Love', duration: '7:53' },
-         { title: 'Killer Queen', duration: '1:59'},
-         { title: "I'm in Love with My Car", duration: '2:03' },
-         { title: 'Get Down, Make Love', duration: '4:45' },
-         { title: 'Save Me', duration: '4:14'},
-         { title: 'Dragon Attack(song)', duration: '3:11' },
-         { title: "Now I'm here", duration: '1:59'},
-         { title: 'Love Of My Life', duration: '3:56' },
-         { title: 'Under Pressure', duration: '3:49' }
-     ]
- };
+var setSong = function(songNumber) {
+    currentlyPlayingSongNumber = parseInt(songNumber);
+    currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+};
 
- var createSongRow = function(songNumber, songName, songLength) {
+var getSongNumberCell = function(number) {
+    return $('.song-item-number[data-song-numbers="' + Number + '"]');
+};
+
+var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
       + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
@@ -60,48 +17,55 @@ var albumQueen = {
       ;
       var $row = $(template);
      
-      var clickHandler = function() {
-          
-          var songNumber = $(this).attr('.data-song-number');
-          
-          if (currentlyPlayingSong !== null) {
-              var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
-              currentlyPlayingCell.html(currentlyPlayingSong);
-          }
-          if (currentlyPlayingSong !== songNumber) {
-              $(this).html(pauseButtonTemplate);
-              currentlyPlayingSong - songNumber;
-          } else if (currentlyPlayingSong === songNumber) {
-              $(this).htl(playButtonTemplate);
-              currentlyPlayingSong = null;
-          }
-      };
-     
-      var onHover = function(event) {
-          var songNumberCell = $(this).find('.song-item-number');
-          var songNumber = songNumberCell.attr('data-song-number');
-          
-          if (songNumber !== currentlyPlayingSong) {
-              songNumberCell.html(playButtonTemplate);
-          }
-          
-      };
-     
-      var offHover = function(event) {
-          var songNumberCell = $(this).find('.song-item-number');
-          var songNumber = songNumberCell.attr('data-song-number');
-          
-          if (songNumber !== currentlyPlayingSong) {
-              songNumberCell.html(songNumber);
-          
-          }
-          
-      };
-     
+ var clickHandler = function() {
+
+     var songNumber = $(this).attr('data-song-number');
+
+    if (currentlyPlayingSongNumber !== null) {
+         
+             var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+             currentlyPlayingCell.html(currentlyPlayingSongNumber);
+     }
+
+    if (currentlyPlayingSongNumber !== songNumber) {
+        // Switch from Play -> Pause button to indicate new song is playing.
+        $(this).html(pauseButtonTemplate);
+        setSong = (songNumber);
+        
+
+    } else if (currentlyPlayingSongNumber === songNumber) {
+        // Switch from Pause -> Play button to pause currently playing song.
+        $(this).html(playButtonTemplate);
+        currentlyPlayingSongNumber = null;
+        currentSongFromAlbum = null;
+     }
+
+ };
+
+ var onHover = function(event) {
+     var songNumberCell = $(this).find('.song-item-number');
+     var songNumber = songNumberCell.attr('data-song-number');
+
+
+     if (songNumber !== currentlyPlayingSongNumber) {
+         songNumberCell.html(playButtonTemplate);
+     }
+ };
+
+ var offHover = function(event) {
+     var songNumberCell = $(this).find('.song-item-number');
+     var songNumber = songNumberCell.attr('data-song-number');
+
+
+     if (songNumber !== currentlyPlayingSongNumber) {
+        songNumberCell.html(songNumber);
+     }
+ };
+        
       $row.find('.song-item-number').click(clickHandler);
       $row.hover(onHover, offHover);
       return $row;
-     
+      console.log("songNumber type is " + typeof songNumber + "\n and currentlyPlayingSongNumber type is " + typeof currentlyPlayingSongNumber);
  };
 
      // drp to global scope
@@ -113,6 +77,7 @@ var albumQueen = {
 
 
  var setCurrentAlbum = function(album) {
+     currentAlbum = album;
      var $albumTitle = $('.album-view-title');
      var $albumArtist = $('.album-view-artist');
      var $albumReleaseInfo = $('.album-view-release-info');
@@ -128,6 +93,68 @@ var albumQueen = {
          $albumSongList.append($newRow);
      }
  };
+
+ var trackIndex = function(album, song) {
+     return album.songs.indexOf(song);
+ };
+
+var updatePlayerBarSong = function() {
+    console.log('I am in here!!!!');
+    $('.currently-playing .song-name').text(currentSongFromAlbum.title);
+    $('.currently-playing .artist-name').text(currentAlbum.artist);
+    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
+
+};
+
+var nextSong = function() {
+    var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+    // Note that we're _incrementing_ the song here
+    currentSongIndex++;
+
+    if (currentSongIndex >= currentAlbum.songs.length) {
+        currentSongIndex = 0;
+    }
+
+    var lastSongNumber = currentlyPlayingSongNumber;
+
+    
+    currentlyPlayingSongNumber = currentSongIndex + 1;
+    currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+
+    updatePlayerBarSong();
+
+    var $nextSongNumberCell = getSongNumberCell(currentlyPlayingSongNumber + currentlyPlayingSongNumber);
+    var $lastSongNumberCell = getSongNumberCell(currentlyPlayingSongNumber + lastSongNumber);
+
+    $nextSongNumberCell.html(pauseButtonTemplate);
+    $lastSongNumberCell.html(lastSongNumber);
+};
+
+var previousSong = function() {
+    var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+
+    currentSongIndex--;
+
+    if (currentSongIndex < 0) {
+        currentSongIndex = currentAlbum.songs.length - 1;
+    }
+
+    var lastSongNumber = currentlyPlayingSongNumber;
+
+    // Set a new current song
+    currentlyPlayingSongNumber = currentSongIndex + 1;
+    currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+
+    updatePlayerBarSong();
+
+    $('.main-controls .play-pause').html(playerBarPauseButton);
+
+    var $previousSongNumberCell = getSongNumberCell(currentlyPlayingSongNumber + currentlyPlayingSongNumber);
+    var $lastSongNumberCell = getSongNumberCell(currentlyPlayingSongNumber + lastSongNumber);
+
+    $previousSongNumberCell.html(pauseButtonTemplate);
+    $lastSongNumberCell.html(lastSongNumber);
+};
 
 var getSongItem = function(element) {
     switch (element.className) {
@@ -147,14 +174,23 @@ var getSongItem = function(element) {
     }  
 };
 
-var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
-var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+ var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+ var playerBarPlayButton = '<span class="ion-play"></span>';
+ var playerBarPauseButton = '<span class="ion-pause"></span>';
 
-//store state of playing songs
-var currentlyPlayingSong = null
-   
+
+ var currentAlbum = null;
+ var currentlyPlayingSongNumber = null;
+ var currentSongFromAlbum = null;
+
+ var $previousButton = $('.main-controls .previous');
+ var $nextButton = $('.main-controls .next');
+
 $(document).ready(function() {
   setCurrentAlbum(albumPicasso);
+  $previousButton.click(previousSong);
+  $nextButton.click(nextSong);
 });
     
          
---
+++ scripts/fixtures.js
@@ -0,0 +1,51 @@
+ var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21' },
+         { title: 'Magenta', duration: '2:15' }
+     ]
+ };
+ 
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21' },
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15' }
+     ]
+ };
+
+ var albumQueen = {
+     title: 'Queen Rock Montreal',
+     artist: 'Queen',
+     label: 'Mountain Studios',
+     year: '1982',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Intro', duration: '1:59' },
+         { title: 'We Will ROck You', duration: '3:06' },
+         { title: 'Play The Game', duration: '3:57'},
+         { title: 'Somebody To Love', duration: '7:53' },
+         { title: 'Killer Queen', duration: '1:59'},
+         { title: "I'm in Love with My Car", duration: '2:03' },
+         { title: 'Get Down, Make Love', duration: '4:45' },
+         { title: 'Save Me', duration: '4:14'},
+         { title: 'Dragon Attack(song)', duration: '3:11' },
+         { title: "Now I'm here", duration: '1:59'},
+         { title: 'Love Of My Life', duration: '3:56' },
+         { title: 'Under Pressure', duration: '3:49' }
+     ]
+ }; 
